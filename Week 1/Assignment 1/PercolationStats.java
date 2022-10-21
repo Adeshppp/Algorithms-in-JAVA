1f7c3a0aaa3fff83554e3685f551413d994fe893
Adesh <adeshp948@gmail.com>
2022-10-21 06:51:30 -0400

    Assignment 1: percolation udated

--- Week 1/Assignment 1/PercolationStats.java
+++ Week 1/Assignment 1/PercolationStats.java
@@ -3,15 +3,20 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private static final double CONFINDENCE_95 = 1.96;
+
+    private static final double CONFIDENCE_95 = 1.96;
     private final int experimentsCount;
     private final double[] fractions;
 
-    public PercolationStats(int n, int trials) {
-        if (n <= 0 || trials <= 0) {
-            throw new IllegalArgumentException();
+    /**
+     * Performs T independent computational experiments on an N-by-N grid.
+     */
+
+    public PercolationStats(int n, int t) {
+        if (n <= 0 || t <= 0) {
+            throw new IllegalArgumentException("Given N <= 0 || T <= 0");
         }
-        experimentsCount = trials;
+        experimentsCount = t;
         fractions = new double[experimentsCount];
         for (int expNum = 0; expNum < experimentsCount; expNum++) {
             Percolation pr = new Percolation(n);
@@ -24,34 +29,51 @@ public class PercolationStats {
                     openedSites++;
                 }
             }
-            double fraction = (double) (openedSites / (n * n));
+            double fraction = (double) openedSites / (n * n);
             fractions[expNum] = fraction;
         }
     }
+
+    /**
+     * Sample mean of percolation threshold.
+     */
 
     public double mean() {
         return StdStats.mean(fractions);
     }
 
+    /**
+     * Sample standard deviation of percolation threshold.
+     */
+
     public double stddev() {
         return StdStats.stddev(fractions);
     }
 
+    /**
+     * Returns lower bound of the 95% confidence interval.
+     */
+
     public double confidenceLo() {
-        return mean() - ((CONFINDENCE_95 * stddev()) / Math.sqrt(experimentsCount));
+        return mean() - ((CONFIDENCE_95 * stddev()) / Math.sqrt(experimentsCount));
     }
 
+    /**
+     * Returns upper bound of the 95% confidence interval.
+     */
+
     public double confidenceHi() {
-        return mean() + ((CONFINDENCE_95 * stddev()) / Math.sqrt(experimentsCount));
+        return mean() + ((CONFIDENCE_95 * stddev()) / Math.sqrt(experimentsCount));
     }
 
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int trials = Integer.parseInt(args[1]);
-        PercolationStats ps = new PercolationStats(n, trials);
-        String confidence = ps.confidenceLo() + "," + ps.confidenceHi();
-        StdOut.println("mean=" + ps.mean());
-        StdOut.println("stddev=" + ps.stddev());
-        StdOut.println("95% confidence interval=" + confidence);
+        int t = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, t);
+
+        String confidence = ps.confidenceLo() + ", " + ps.confidenceHi();
+        StdOut.println("mean                    = " + ps.mean());
+        StdOut.println("stddev                  = " + ps.stddev());
+        StdOut.println("95% confidence interval = " + confidence);
     }
 }
